acd78ec41375e5246643dfcd31634a4e1527ee90
hojoo <hojoolee81@gmail.com>
2020-03-27 11:10:53 -0700

    1st commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GithubActions2Azure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
